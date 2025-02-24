a2d2f6143d7a149ea347eb200bd2972b69d1793a
Dipak Singh <140516704+DipakSingh0@users.noreply.github.com>
2025-02-24 09:09:45 +0530

    first commit

---
+++ lib/view/splash_view.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:music_player/view_model/splash_view_model.dart'; // Import for SystemChrome
+
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+
+  final splashVM = Get.put(SplashViewModel());
+
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.leanBack);
+    splashVM.loadView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.sizeOf(context);
+    return  Scaffold(
+      body: Center(child: Image.asset("assets/img/app_logo.png", width:size.width * 0.5)),
+    ); 
+  }
+}
